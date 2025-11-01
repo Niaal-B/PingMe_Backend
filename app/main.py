4fa4243ab814f47a306e6bc14c61acddff6e41d4
Niaal-B <nihnihal504@gmail.com>
2025-11-01 12:51:18 +0530

    removed all files to start from fresh

--- app/main.py
+++ app/main.py
@@ -1,43 +1,7 @@
-
-
 from fastapi import FastAPI
-from dotenv import load_dotenv
-from fastapi.middleware.cors import CORSMiddleware
-from contextlib import asynccontextmanager
-from app.api import auth ,dashboard,rooms,chat,websocket_test
-import asyncio
 
-load_dotenv()
-
-
-    
-
-
-app = FastAPI(
-    title="PingMe API",
-    description="Backend for the real-time chat application.",
-    version="1.0.0"
-)
-
-origins = [
-    "http://localhost:5173",  
-    "http://127.0.0.1:5173",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,  
-    allow_credentials=True,
-    allow_methods=["*"],    
-    allow_headers=["*"],  
-)
-
-app.include_router(auth.router)
-app.include_router(dashboard.router)
-app.include_router(rooms.router)
-app.include_router(websocket_test.router)
-
+app = FastAPI(title="FastAPI Chat App")
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to PingMe! FastAPI is running."}
+    return {"message": "Welcome to the FastAPI Chat App!"}
